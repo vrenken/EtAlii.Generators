c7ced11fd295e824b5b82be3105572ea3e06319a
Peter Vrenken <peter.vrenken@atos.net>
2021-03-23 00:03:26 +0100

    Abstracted WriteContext creation out of SourceGenerator and into a WriteContextFactory.

--- Source/EtAlii.Generators.Stateless/SourceGenerator.cs
+++ Source/EtAlii.Generators.Stateless/SourceGenerator.cs
@@ -82,7 +82,7 @@
 
                         using var stringWriter = new StringWriter();
                         using var writer = new IndentedTextWriter(stringWriter);
-                        var writeContext = CreateWriteContext(writer, originalFileName, log, stateMachine);
+                        var writeContext = new WriteContextFactory().Create(writer, originalFileName, log, stateMachine);
 
                         _plantUmlValidator.Validate(writeContext, diagnostics);
 
@@ -114,63 +114,6 @@
             log = log.SelectMany(l => l.Replace("\r\n", "\n").Split(new[] { "\n" }, StringSplitOptions.None)).ToList();
             log = log.Select(l => $"// {l}").ToList();
             context.AddSource( "Log.txt", string.Join("\n",log));
-        }
-
-        /// <summary>
-        /// Create a context with commonly used instances and data that we can easily pass through the whole writing callstack.
-        /// </summary>
-        private WriteContext CreateWriteContext(IndentedTextWriter writer, string originalFileName, List<string> log, StateMachine stateMachine)
-        {
-            var allTransitions = stateMachine.StateFragments
-                .OfType<Transition>()
-                .ToArray();
-
-            log.Add("Transitions found:");
-            log.AddRange(allTransitions.Select(t =>
-            {
-                var parameters = t.Parameters.Any()
-                    ? $" ({string.Join(", ", t.Parameters.Select(p => $"{p.Type} {p.Name}".Trim()))}) "
-                    : " ";
-
-                var asyncPrefix = t.IsAsync ? "async " : "";
-                var stereoType = t.IsAsync || t.Parameters.Any()
-                    ? $" << {asyncPrefix}{parameters}>> "
-                    : " ";
-                return $"- {t.From} -> {t.To}{stereoType}: {t.Trigger}";
-            }));
-
-            // We want to know all unique states defined in the diagram.
-            var transitionStates = allTransitions.SelectMany(t => new[] { t.From, t.To });
-            var descriptionStates = stateMachine.StateFragments.OfType<StateDescription>().SelectMany(t => new[] { t.State });
-            var allStates = transitionStates
-                .Concat(descriptionStates)
-                .Where(s => s != null)
-                .OrderBy(s => s)
-                .Distinct() // That is, of course without any doubles.
-                .ToArray();
-
-            log.Add("States found:");
-            log.AddRange(allStates.Select(s => $"- {s}"));
-
-            // We want to know all unique triggers defined in the diagram.
-            var transitionTriggers = allTransitions.Select(t => t.Trigger);
-            var allTriggers = transitionTriggers
-                .OrderBy(t => t)
-                .Distinct() // That is, of course without any doubles.
-                .ToArray();
-
-            log.Add("Triggers found:");
-            log.AddRange(allTriggers.Select(t => $"- {t}"));
-
-            // We want to know all unique transitions defined in the diagram.
-            // That is, the transitions grouped by the trigger and unique sequence of parameters.
-            var uniqueParameterTransitions = allTransitions
-                .Select(t => new { Transition = t, ParametersAsKey = $"{t.Trigger}{string.Join(", ", t.Parameters.Select(p => p.Type))}" })
-                .GroupBy(item => item.ParametersAsKey)
-                .Select(g => g.First().Transition)
-                .ToArray();
-
-            return new WriteContext(writer, originalFileName, stateMachine, allStates, allTriggers, allTransitions, uniqueParameterTransitions);
         }
 
         public void Initialize(GeneratorInitializationContext context)
---
+++ Source/EtAlii.Generators.Stateless/WriteContextFactory.cs
@@ -0,0 +1,66 @@
+namespace EtAlii.Generators.Stateless
+{
+    using System.CodeDom.Compiler;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class WriteContextFactory
+    {
+        /// <summary>
+        /// Create a context with commonly used instances and data that we can easily pass through the whole writing callstack.
+        /// </summary>
+        public WriteContext Create(IndentedTextWriter writer, string originalFileName, List<string> log, StateMachine stateMachine)
+        {
+            var allTransitions = stateMachine.StateFragments
+                .OfType<Transition>()
+                .ToArray();
+
+            log.Add("Transitions found:");
+            log.AddRange(allTransitions.Select(t =>
+            {
+                var parameters = t.Parameters.Any()
+                    ? $" ({string.Join(", ", t.Parameters.Select(p => $"{p.Type} {p.Name}".Trim()))}) "
+                    : " ";
+
+                var asyncPrefix = t.IsAsync ? "async " : "";
+                var stereoType = t.IsAsync || t.Parameters.Any()
+                    ? $" << {asyncPrefix}{parameters}>> "
+                    : " ";
+                return $"- {t.From} -> {t.To}{stereoType}: {t.Trigger}";
+            }));
+
+            // We want to know all unique states defined in the diagram.
+            var transitionStates = allTransitions.SelectMany(t => new[] { t.From, t.To });
+            var descriptionStates = stateMachine.StateFragments.OfType<StateDescription>().SelectMany(t => new[] { t.State });
+            var allStates = transitionStates
+                .Concat(descriptionStates)
+                .Where(s => s != null)
+                .OrderBy(s => s)
+                .Distinct() // That is, of course without any doubles.
+                .ToArray();
+
+            log.Add("States found:");
+            log.AddRange(allStates.Select(s => $"- {s}"));
+
+            // We want to know all unique triggers defined in the diagram.
+            var transitionTriggers = allTransitions.Select(t => t.Trigger);
+            var allTriggers = transitionTriggers
+                .OrderBy(t => t)
+                .Distinct() // That is, of course without any doubles.
+                .ToArray();
+
+            log.Add("Triggers found:");
+            log.AddRange(allTriggers.Select(t => $"- {t}"));
+
+            // We want to know all unique transitions defined in the diagram.
+            // That is, the transitions grouped by the trigger and unique sequence of parameters.
+            var uniqueParameterTransitions = allTransitions
+                .Select(t => new { Transition = t, ParametersAsKey = $"{t.Trigger}{string.Join(", ", t.Parameters.Select(p => p.Type))}" })
+                .GroupBy(item => item.ParametersAsKey)
+                .Select(g => g.First().Transition)
+                .ToArray();
+
+            return new WriteContext(writer, originalFileName, stateMachine, allStates, allTriggers, allTransitions, uniqueParameterTransitions);
+        }
+    }
+}
