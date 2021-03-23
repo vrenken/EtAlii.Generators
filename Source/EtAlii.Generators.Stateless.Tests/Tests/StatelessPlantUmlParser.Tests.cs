4b77092a2866be15fe15756e00784d26ea427caf
Peter Vrenken <peter.vrenken@atos.net>
2021-03-23 21:14:22 +0100

    Added test for invalid parsing.

--- Source/EtAlii.Generators.Stateless.Tests/Tests/StatelessPlantUmlParser.Tests.cs
+++ Source/EtAlii.Generators.Stateless.Tests/Tests/StatelessPlantUmlParser.Tests.cs
@@ -18,7 +18,7 @@
         }
 
         [Fact]
-        public void StatelessPlantUmlParser_TryParse()
+        public void StatelessPlantUmlParser_TryParse_Valid()
         {
 
             // Arrange.
@@ -47,7 +47,37 @@ State2 -up-> State3 : Continue
             Assert.Equal("EtAlii.Generators.Stateless.Tests", stateMachine.Namespace);
             Assert.Equal("MyFancyStateMachineBase", stateMachine.ClassName);
             Assert.NotNull(diagnostics);
+            Assert.Empty(diagnostics);
         }
 
+
+        [Fact]
+        public void StatelessPlantUmlParser_TryParse_Invalid()
+        {
+            // Arrange.
+            var parser = new StatelessPlantUmlParser();
+            var log = new List<string>();
+
+            var text = @"@startuml
+'stateless namespace EtAlii.Generators.Stateless.Tests
+'stateless class MyFancyStateMachineBase
+'stateless generate partial
+'stateless using System.Text
+
+[*] -> State1 << (string name) >> : Start
+State1 -> State2 << (string name) >INVALID> : Continue
+State2 -> State2 << async (string name) >> : Check
+State2 -up-> State3 : Continue
+@enduml";
+            var file = new TestAdditionalTextFile(text, "Test.puml");
+
+            // Act.
+            var result = parser.TryParsePlantUml(file, log, out var stateMachine, out var diagnostics);
+
+            // Assert.
+            Assert.False(result);
+            Assert.Null(stateMachine);
+            Assert.NotEmpty(diagnostics);
+        }
     }
 }
