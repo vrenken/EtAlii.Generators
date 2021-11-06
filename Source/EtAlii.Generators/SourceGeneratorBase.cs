38c78c48580196cd27c8992e5aeb407f1c3b4933
Peter Vrenken <peter.vrenken@atos.net>
2021-11-06 19:20:33 +0100

    Fixed correlation ID.

--- Source/EtAlii.Generators/SourceGeneratorBase.cs
+++ Source/EtAlii.Generators/SourceGeneratorBase.cs
@@ -27,88 +27,90 @@ namespace EtAlii.Generators
 
         public void Execute(GeneratorExecutionContext context)
         {
-            using (Correlation.Begin(CorrelationType.SourceGeneration))
             // For testing and troubleshooting we'll use localised Seq logging. This should become deactivated outside of the primary development system.
             SetupLogging();
 
-            // For actual troubleshooting of the source diagrams we use the Roslyn specific Diagnostics pattern.
-            var diagnostics = new List<Diagnostic>();
-
-            var extension = GetExtension();
-            var sourceItemGroup = GetSourceItemGroup();
-            var parsingExceptionRule = GetParsingExceptionRule();
-
-            // This code generator is only able to understand and parse the designated files.
-            // Because of that we ignore everything except files with the configured extension.
-            var additionalFiles = context.AdditionalFiles
-                .Where(file => Path.GetExtension(file.Path).Equals(extension, StringComparison.OrdinalIgnoreCase))
-                .Where(f =>
-                {
-                    try
-                    {
-                        var options = context.AnalyzerConfigOptions.GetOptions(f);
-                        if (options.TryGetValue(SourceItemGroupMetadata, out var sourceItemGroupMetadata))
-                        {
-                            _log.Information("Found {SourceItemGroup} {SourceItemGroupMetadata}", Path.GetFileName(f.Path), sourceItemGroupMetadata);
-                            return sourceItemGroupMetadata.Equals(sourceItemGroup, StringComparison.OrdinalIgnoreCase);
-                        }
-                        else
-                        {
-                            _log.Error("Failed finding {SourceItemGroup}", Path.GetFileName(f.Path));
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        _log.Fatal(e, "Exception while finding SourceItemGroup");
-                    }
-                    return false;
-                })
-                .ToArray();
-
-            var parser = CreateParser();
-            var writerFactory = CreateWriterFactory();
-            var writer = writerFactory.Create();
-            var validator = CreateValidator();
-
-            foreach(var file in additionalFiles)
+            using (Correlation.Begin(CorrelationType.SourceGeneration))
             {
-                // First thing to do is parse the file and build an in-memory model.
-                if (parser.TryParse(file, out var instance, out var parseDiagnostics))
-                {
-                    try
+                // For actual troubleshooting of the source diagrams we use the Roslyn specific Diagnostics pattern.
+                var diagnostics = new List<Diagnostic>();
+
+                var extension = GetExtension();
+                var sourceItemGroup = GetSourceItemGroup();
+                var parsingExceptionRule = GetParsingExceptionRule();
+
+                // This code generator is only able to understand and parse the designated files.
+                // Because of that we ignore everything except files with the configured extension.
+                var additionalFiles = context.AdditionalFiles
+                    .Where(file => Path.GetExtension(file.Path).Equals(extension, StringComparison.OrdinalIgnoreCase))
+                    .Where(f =>
                     {
-                        var originalFileName = Path.GetFileName(file.Path);
-                        var fullPathToFile = file.Path;
-                        var fileName = Path.ChangeExtension(originalFileName, "Generated.cs");
+                        try
+                        {
+                            var options = context.AnalyzerConfigOptions.GetOptions(f);
+                            if (options.TryGetValue(SourceItemGroupMetadata, out var sourceItemGroupMetadata))
+                            {
+                                _log.Information("Found {SourceItemGroup} {SourceItemGroupMetadata}", Path.GetFileName(f.Path), sourceItemGroupMetadata);
+                                return sourceItemGroupMetadata.Equals(sourceItemGroup, StringComparison.OrdinalIgnoreCase);
+                            }
+                            else
+                            {
+                                _log.Error("Failed finding {SourceItemGroup}", Path.GetFileName(f.Path));
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            _log.Fatal(e, "Exception while finding SourceItemGroup");
+                        }
+                        return false;
+                    })
+                    .ToArray();
 
-                        validator.Validate(instance, fullPathToFile, diagnostics);
+                var parser = CreateParser();
+                var writerFactory = CreateWriterFactory();
+                var writer = writerFactory.Create();
+                var validator = CreateValidator();
 
-                        using var stringWriter = new StringWriter();
-                        using var indentedWriter = new IndentedTextWriter(stringWriter);
-
-                        _log.Information("Creating WriteContext");
-                        var writeContext = CreateWriteContext(instance, indentedWriter, originalFileName);
-                        writer.Write(writeContext);
-
-                        var content = stringWriter.ToString();
-                        context.AddSource(fileName, content);
-                    }
-                    catch (Exception e)
-                    {
-                        _log.Fatal(e, "File writing threw an exception");
-
-                        var location = Location.Create(file.Path, new TextSpan(), new LinePositionSpan());
-                        var diagnostic = Diagnostic.Create(parsingExceptionRule, location, e.Message, e.StackTrace);
-                        diagnostics.Add(diagnostic);
-                    }
-                }
-
-                diagnostics.AddRange(parseDiagnostics);
-
-                // Report any error/info, warning etc.
-                foreach (var diagnostic in diagnostics)
+                foreach(var file in additionalFiles)
                 {
-                    context.ReportDiagnostic(diagnostic);
+                    // First thing to do is parse the file and build an in-memory model.
+                    if (parser.TryParse(file, out var instance, out var parseDiagnostics))
+                    {
+                        try
+                        {
+                            var originalFileName = Path.GetFileName(file.Path);
+                            var fullPathToFile = file.Path;
+                            var fileName = Path.ChangeExtension(originalFileName, "Generated.cs");
+
+                            validator.Validate(instance, fullPathToFile, diagnostics);
+
+                            using var stringWriter = new StringWriter();
+                            using var indentedWriter = new IndentedTextWriter(stringWriter);
+
+                            _log.Information("Creating WriteContext");
+                            var writeContext = CreateWriteContext(instance, indentedWriter, originalFileName);
+                            writer.Write(writeContext);
+
+                            var content = stringWriter.ToString();
+                            context.AddSource(fileName, content);
+                        }
+                        catch (Exception e)
+                        {
+                            _log.Fatal(e, "File writing threw an exception");
+
+                            var location = Location.Create(file.Path, new TextSpan(), new LinePositionSpan());
+                            var diagnostic = Diagnostic.Create(parsingExceptionRule, location, e.Message, e.StackTrace);
+                            diagnostics.Add(diagnostic);
+                        }
+                    }
+
+                    diagnostics.AddRange(parseDiagnostics);
+
+                    // Report any error/info, warning etc.
+                    foreach (var diagnostic in diagnostics)
+                    {
+                        context.ReportDiagnostic(diagnostic);
+                    }
                 }
             }
         }
